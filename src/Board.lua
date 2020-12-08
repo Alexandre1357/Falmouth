632344dc205658392462de4a746c4018f01eee7d
Alexandre1357 <70177765+Alexandre1357@users.noreply.github.com>
2020-12-08 15:25:38 -0500

    update board creation system
    new way to check which varieties can spawn

--- src/Board.lua
+++ src/Board.lua
@@ -17,11 +17,15 @@ function Board:init(x, y, level)
     self.x = x
     self.y = y
     self.matches = {}
+    self.variety = level
+    if level > 6 then
+        self.variety = 6
+    end
 
-    self:initializeTiles(level)
+    self:initializeTiles()
 end
 
-function Board:initializeTiles(level)
+function Board:initializeTiles()
     self.tiles = {}
 
     for tileY = 1, 8 do
@@ -30,9 +34,9 @@ function Board:initializeTiles(level)
         table.insert(self.tiles, {})
 
         for tileX = 1, 8 do
-            
+
             -- create a new tile at X,Y with a random color and variety
-            table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(18), math.random(level)))
+            table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(18), math.random(self.variety)))
         end
     end
 
@@ -240,7 +244,7 @@ function Board:getFallingTiles()
             if not tile then
 
                 -- new tile with random color and variety
-                local tile = Tile(x, y, math.random(18), math.random(6))
+                local tile = Tile(x, y, math.random(18), math.random(self.variety))
                 tile.y = -32
                 self.tiles[y][x] = tile
 
